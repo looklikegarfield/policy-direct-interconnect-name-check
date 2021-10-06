a68540ef09b11d1264930fe5958e64e7cc608314
looklikegarfield <looklikegarfieldd@gmail.com>
2021-10-06 13:30:06 +0530

    Create interconnect.tf

---
+++ interconnect.tf
@@ -0,0 +1,49 @@
+resource "google_compute_interconnect_attachment" "on_prem" {
+  name                     = "on-prem-attachment"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+  type                     = "PARTNER"
+  router                   = google_compute_router.foobar.id
+  mtu                      = 1500
+  encryption               = "IPSEC"
+  ipsec_internal_addresses = [
+    google_compute_address.address.self_link,
+  ]
+}
+
+#resource "google_compute_router" "foobar" {
+#  name    = "router"
+#  network = google_compute_network.foobar.name
+#  bgp {
+#    asn = 16550
+#  }
+#}
+
+resource "google_compute_address" "address" {
+  name          = "test-address"
+  address_type  = "INTERNAL"
+  purpose       = "IPSEC_INTERCONNECT"
+  address       = "192.168.1.0"
+  prefix_length = 29
+  network       = google_compute_network.foobar.self_link
+}
+
+resource "google_compute_network" "foobar" {
+  name                    = "network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_router" "foobar" {
+  name    = "my-router"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn               = 64514
+    advertise_mode    = "CUSTOM"
+    advertised_groups = ["ALL_SUBNETS"]
+    advertised_ip_ranges {
+      range = "1.2.3.4"
+    }
+    advertised_ip_ranges {
+      range = "6.7.0.0/16"
+    }
+  }
+}
